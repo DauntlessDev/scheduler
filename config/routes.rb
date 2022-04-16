0a2892d069228d53ee50dfa0d004d4fdfecfc1a4
dauntlessdev <45929622+DauntlessDev@users.noreply.github.com>
2022-04-16 14:48:23 +0800

    re push

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  root "schedule#index"
+
+  devise_for :users do
+    resources :schedule
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
